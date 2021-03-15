0cec02199b590b14622087d5df66759d5b8bdd07
Danizold34 <79314048+Danizold34@users.noreply.github.com>
2021-03-15 23:15:13 +0300

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats( matrix ) {
   let count = 0;
   for( let i = 0; i < matrix.length; i++){
   	for(let j = 0; j < matrix[i].length; j++){
