94b9d18ad9a2840a1955a229dba8a5e8c932399b
Danizold34 <79314048+Danizold34@users.noreply.github.com>
2021-03-15 23:12:41 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,6 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity ) {
-  throw new CustomError('Not implemented');
    if(isNaN(sampleActivity || typeof sampleActivity !== 'string' || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY)){ return false;} 
   let temporary = 0.693/HALF_LIFE_PERIOD;
   return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) / temporary);
