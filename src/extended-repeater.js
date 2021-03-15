6153777c221f3f7a29d916108712b21eb57db7c6
Danizold34 <79314048+Danizold34@users.noreply.github.com>
2021-03-15 23:20:43 +0300

    update

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+ let tmp = "";
+  options.repeatTimes === undefined ? options.repeatTimes = 1 : options.repeatTimes;
+  options.separator === undefined ? options.separator = "+" : options.separator;
+  options.addition === undefined ? options.addition = "" : options.addition;
+  options.additionRepeatTimes === undefined ? options.additionRepeatTimes = 1 : options.additionRepeatTimes;
+  options.additionSeparator === undefined ? options.additionSeparator = "|" : options.additionSeparator;
+
+  function getRepeatTimes(num, s) {
+    let resChain = "";
+    for (let i = 0; i < num; i++) {  
+      if(i === num - 1) resChain += str + getAdditionRepeatTimes(options.addition, options.additionRepeatTimes, options.additionSeparator);
+
+      else resChain += str + getAdditionRepeatTimes(options.addition, options.additionRepeatTimes, options.additionSeparator) + s;
+    }
+    return resChain;
+  }
+
+  function getAdditionRepeatTimes(add, num, st) {
+    
+    let res = "";
+    
+    for (let i = 0; i < num; i++) {
+      if(i === num - 1){
+        res += add;
+      }
+      else res += add + st;
+    }
+    return res;
+  }
+  
+  return tmp += getRepeatTimes(options.repeatTimes, options.separator);
+  
 };
   
